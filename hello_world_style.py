945d57e5c91f03245f49355b1e64a68930d0e098
Noah-234 <noah.ouellette@stu.ocsb.ca>
2021-11-24 05:54:43 +0000

    Created and finished Hello World program with proper style

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Created By: Noah Ouellette
+# Date: Nov. 24, 2021
+# This program prints "Hello World!" with proper Python3 style
+
+# Defines the function and what it will execute.
+
+def main():
+    print("Hello World! Now with proper style included!")
+
+
+if __name__ == "__main__":
+    main()
